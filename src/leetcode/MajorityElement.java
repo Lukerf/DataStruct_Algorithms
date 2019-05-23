5809cbf891435b02bb2174e532626277c6b4d07a
Your Name <email@example.com>
2019-05-23 18:40:26 +0800

    yxg

---
+++ src/leetcode/MajorityElement.java
@@ -0,0 +1,58 @@
+/**
+求众数
+给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+
+你可以假设数组是非空的，并且给定的数组总是存在众数。
+
+示例 1:
+
+输入: [3,2,3]
+输出: 3
+示例 2:
+
+输入: [2,2,1,1,1,2,2]
+输出: 2
+*/
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class MajorityElement {
+	public static void main(String[] args)
+	{
+		int[] input = {2,2,2,2,1,1,1};
+		MajorityElement maj = new MajorityElement(input);
+		System.out.println(maj.maxNum);
+	}
+	public int maxNum;
+	MajorityElement(int[] input)
+	{
+		int length = input.length;
+		HashMap map = new HashMap();
+		this.maxNum = input[0];
+		for(int i = 0;i<length;i++)
+		{
+			if(map.containsKey(input[i]))
+			{
+				map.put(input[i],(int)map.get(input[i])+1);
+			}
+			else
+			{
+				map.put(input[i],1);
+			}
+		}
+		Iterator iter = map.entrySet().iterator();
+		while(iter.hasNext())
+		{
+			Map.Entry ent = (Map.Entry)iter.next();
+			if((int)ent.getValue()>length/2)
+			{
+	
+				this.maxNum = (int)ent.getKey();
+			}
+		}
+	}
+
+}
